20ddea4c4b7c99e089ed77858671ce1ab46f95df
szobonyaerik <55093892+szobonyaerik@users.noreply.github.com>
2019-09-12 15:24:21 +0200

    Add files via upload

---
+++ ideabank.py
@@ -0,0 +1,9 @@
+idea = input("Write your idea here: " )
+
+filename = open("ideas.txt", "a")
+filename.write(idea)
+filename.write( "\n")
+filename.close()
+
+filename = open("ideas.txt" , "r")
+print(filename.read())
